567aadee4055f3e39a73fb2b313a67196092af23
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-05-31 22:34:30 +0300

    GUI интерфейс для главного окна

---
+++ gui/win_gui.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'win_gui.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(850, 426)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+
+        self.label_title = QLabel(self.frame)
+        self.label_title.setObjectName(u"label_title")
+        font = QFont()
+        font.setFamily(u"Times New Roman")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_title.setFont(font)
+
+        self.horizontalLayout.addWidget(self.label_title)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setSpacing(3)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setSpacing(7)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.btn_add = QPushButton(self.frame)
+        self.btn_add.setObjectName(u"btn_add")
+
+        self.horizontalLayout_3.addWidget(self.btn_add)
+
+        self.btn_remove = QPushButton(self.frame)
+        self.btn_remove.setObjectName(u"btn_remove")
+
+        self.horizontalLayout_3.addWidget(self.btn_remove)
+
+        self.btn_update = QPushButton(self.frame)
+        self.btn_update.setObjectName(u"btn_update")
+
+        self.horizontalLayout_3.addWidget(self.btn_update)
+
+
+        self.horizontalLayout_4.addLayout(self.horizontalLayout_3)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_3)
+
+        self.line_search = QLineEdit(self.frame)
+        self.line_search.setObjectName(u"line_search")
+
+        self.horizontalLayout_4.addWidget(self.line_search)
+
+        self.btn_search = QPushButton(self.frame)
+        self.btn_search.setObjectName(u"btn_search")
+
+        self.horizontalLayout_4.addWidget(self.btn_search)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.verticalLayout.addItem(self.horizontalSpacer_5)
+
+
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.tableWidget = QTableWidget(self.frame)
+        if (self.tableWidget.columnCount() < 6):
+            self.tableWidget.setColumnCount(6)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(5, __qtablewidgetitem5)
+        self.tableWidget.setObjectName(u"tableWidget")
+
+        self.horizontalLayout_2.addWidget(self.tableWidget)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_name = QLabel(self.frame)
+        self.label_name.setObjectName(u"label_name")
+        self.label_name.setEnabled(False)
+        font1 = QFont()
+        font1.setFamily(u"Z003")
+        font1.setPointSize(12)
+        font1.setItalic(True)
+        self.label_name.setFont(font1)
+
+        self.horizontalLayout_5.addWidget(self.label_name)
+
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_5.addItem(self.horizontalSpacer_4)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_5)
+
+
+        self.verticalLayout_2.addWidget(self.frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Stock", None))
+        self.label_title.setText(QCoreApplication.translate("MainWindow", u"Stock ver: 0.1", None))
+        self.btn_add.setText(QCoreApplication.translate("MainWindow", u"Add", None))
+        self.btn_remove.setText(QCoreApplication.translate("MainWindow", u"Remove", None))
+        self.btn_update.setText(QCoreApplication.translate("MainWindow", u"Update", None))
+        self.btn_search.setText(QCoreApplication.translate("MainWindow", u"Search", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Title", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Data", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Inventory number", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"Count", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"Price", None));
+        ___qtablewidgetitem5 = self.tableWidget.horizontalHeaderItem(5)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"State", None));
+        self.label_name.setText(QCoreApplication.translate("MainWindow", u"By Krupeichenko Victor", None))
+    # retranslateUi
+

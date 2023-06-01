b5a3fdcd31efceb1bdc4222a15940f747a68d016
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-01 11:12:21 +0300

    Создание/подключение к БД

--- app/run.py
+++ app/run.py
@@ -1,9 +1,11 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from app.main import MyStock
+from database import create_db, connect_db
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    create_db.Base.metadata.create_all(bind=connect_db.engine)
     main_prog = MyStock()
     main_prog.showMaximized()
     sys.exit(app.exec_())

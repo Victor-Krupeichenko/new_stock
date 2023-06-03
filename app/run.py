869ab9beaf97031cc44d612eb2f3d239008eef53
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-06-03 11:40:30 +0300

    Запускает приложение

--- app/run.py
+++ app/run.py
@@ -1,11 +1,12 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from app.main import MyStock
-from database import create_db, connect_db
+from database import create_db
+from database.connect_db import engine
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    create_db.Base.metadata.create_all(bind=connect_db.engine)
+    create_db.Base.metadata.create_all(bind=engine)
     main_prog = MyStock()
     main_prog.showMaximized()
     sys.exit(app.exec_())
